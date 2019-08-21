775e6140b7692aedd7ffb7d9414c1c2b9a26591b
DC0509 <54338754+DC0509@users.noreply.github.com>
2019-08-20 22:17:40 -0300

    Adicionei arquivos via upload da Super Máquina

---
+++ Super_Máquina_-_Sequencial_de_LEDs.ino
@@ -0,0 +1,12 @@
+/**
+ * Sequencial de LEDs (efeito supermáquina)
+ * @author - Daniel Casimiro Filho
+ */
+
+void setup() {
+  pinMode(13, OUTPUT);
+  }
+
+void loop() {
+  digitalWrite(13, HIGH);
+  }
